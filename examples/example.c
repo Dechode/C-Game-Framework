78344a87acb85d2baa251c51288ff99b0f279cc4
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-03-23 21:39:25 +0200

    Use physics.h in example, collision response not working yet

--- examples/example.c
+++ examples/example.c
@@ -1,4 +1,5 @@
 #include "../src/renderer.h"
+#include "../src/physics.h"
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_keycode.h>
 #include <SDL2/SDL_version.h>
@@ -73,7 +74,7 @@ void handleInputEvents(SDL_Event* event, InputState* inputState)
 
 typedef struct
 {
-	vec2 position;
+	Body body;
 	vec2 size;
 	int enabled;
 	vec4 color;
@@ -86,14 +87,21 @@ void initBlock(Block* block, vec4 color, vec2 size, vec2 position, int enabled)
 	block->color[2] = color[2];
 	block->color[3] = color[3];
 
-	block->size[0] =size[0] ;
-	block->size[1] =size[1] ;
+	block->size[0] = size[0];
+	block->size[1] = size[1];
+	block->body.aabb.halfSize[0] = 0.5f * block->size[0];
+	block->body.aabb.halfSize[1] = 0.5f * block->size[1];
 
-	block->position[0] = position[0]; 
-	block->position[1] = position[1]; 
+	block->body.aabb.position[0] = position[0]; 
+	block->body.aabb.position[1] = position[1]; 
+
+	block->body.velocity[0] = 0.0f;
+	block->body.velocity[1] = 0.0f;
+
+	block->body.acceleration[0] = 0.0f;
+	block->body.acceleration[1] = 0.0f;
 
 	block->enabled = enabled;
-
 }
 
 void run(SDL_Window* window)
@@ -123,15 +131,21 @@ void run(SDL_Window* window)
 
 	Block ball;
 	initBlock(&ball, (vec4){1.0f, 1.0f, 1.0f, 1.0f}, (vec2){10.0f, 10.0f},(vec2){0.5f * WIDTH, 60.0f}, 1);
+	ball.body.velocity[0] = 1.0f;
+	ball.body.velocity[1] = 1.0f;
+	
 
 	InputState inputState;
 	SDL_Event event;
 
 	struct timeval t0, t1;
 	float deltaTime = 0.0f;
+	float fps = 0.0f;
 
+	// Gameloop
 	while (!shouldQuit)
 	{
+		// Start time of the frame
 		gettimeofday(&t0,0);
 
 		// Event handling
@@ -139,33 +153,68 @@ void run(SDL_Window* window)
 		
 		// Update game entities
 		if (inputState.Left == PRESSED)
-			pad.position[0] -= 500.0f * deltaTime;
+			pad.body.aabb.position[0] -= 500.0f * deltaTime;
 		if (inputState.right == PRESSED)
-			pad.position[0] += 500.0f * deltaTime;
+			pad.body.aabb.position[0] += 500.0f * deltaTime;
 
-		ball.position[1] += 5.0f;
+		ball.body.aabb.position[1] += ball.body.velocity[1] * 350.0f * deltaTime;
+		ball.body.aabb.position[0] += ball.body.velocity[0] * 350.0f * deltaTime;
 
 		// Collision checking
-		
+		for (int i = 0; i < BLOCK_AMOUNT; i++)
+		{
+			if (!blocks[i].enabled)
+				continue;
+
+		    if (physicsAABBIntersectAABB(blocks[i].body.aabb, ball.body.aabb))
+			{
+				ball.body.velocity[0] *= -1;
+				ball.body.velocity[1] *= -1;
+				blocks[i].enabled = 0;
+			}
+		}
+
+		if (ball.body.aabb.position[0] < 0 || ball.body.aabb.position[0] + ball.size[0] > WIDTH)
+		{
+			ball.body.velocity[0] *= -1;
+			ball.body.aabb.position[0] += ball.body.velocity[0] * 450.0f * deltaTime;
+		}
+
+		if (ball.body.aabb.position[1] < 0 || ball.body.aabb.position[1] + ball.size[1] > HEIGHT)
+		{
+			ball.body.velocity[1] *= -1;
+			ball.body.aabb.position[1] += ball.body.velocity[1] * 450.0f * deltaTime;
+		}
+
+		if (physicsAABBIntersectAABB(ball.body.aabb, pad.body.aabb))
+		{
+			ball.body.velocity[0] *= -1;
+			ball.body.velocity[1] *= -1;
+		}
 
 		// Rendering
 		renderBegin();
 
-		renderQuad(0, pad.position, pad.size, pad.color);
-		renderQuad(0, ball.position, ball.size, ball.color);
+		renderQuad(0, pad.body.aabb.position, pad.size, pad.color);
+		renderQuad(0, ball.body.aabb.position, ball.size, ball.color);
 
 		for (int i = 0; i < BLOCK_AMOUNT; i++)
 		{
 			if (!blocks[i].enabled)
 				continue;
-			renderQuad(0, blocks[i].position, blocks[i].size, blocks[i].color);
+			renderQuad(0, blocks[i].body.aabb.position, blocks[i].size, blocks[i].color);
 		}
 
-		renderEnd(window);
+		// End time of the frame and delta time
 		gettimeofday(&t1,0);
 		deltaTime = (t1.tv_sec - t0.tv_sec) * 1000.0f + (t1.tv_usec - t0.tv_usec) / 1000.0f;
 		deltaTime *= 0.001f;
-		printf("deltaTime = %f\n", deltaTime);
+
+		fps = 1 / deltaTime;
+//		printf("FPS = %2.2f\n", fps);
+
+		renderEnd(window);
+
 	}
 
 	// Cleanup
