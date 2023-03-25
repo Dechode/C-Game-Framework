a7417b678ed88fcfca4bdf1c59becb20c82c067a
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-03-25 08:31:39 +0200

    Call input handling from event handler

--- src/input.c
+++ src/input.c
@@ -2,46 +2,42 @@
 
 void handleInputEvents(SDL_Event* event, InputState* inputState)
 {
-	while (SDL_PollEvent(event))
+//	while (SDL_PollEvent(event))
+//	{
+	switch (event->type)
 	{
-		switch (event->type)
-		{
-			case SDL_QUIT:
-				inputState->escape = PRESSED;
-				break;
+		case SDL_KEYDOWN:
+			 switch( event->key.keysym.sym ){
+                case SDLK_LEFT:
+                    inputState->Left = PRESSED;
+                    break;
+                case SDLK_RIGHT:
+                    inputState->right = PRESSED;
+                    break;
+				case SDLK_ESCAPE:
+					inputState->escape = PRESSED;
+					break;
+                default:
+                    break;
+				}
+			break;
 
-			case SDL_KEYDOWN:
-				 switch( event->key.keysym.sym ){
-                    case SDLK_LEFT:
-                        inputState->Left = PRESSED;
-                        break;
-                    case SDLK_RIGHT:
-                        inputState->right = PRESSED;
-                        break;
-					case SDLK_ESCAPE:
-						inputState->escape = PRESSED;
-						break;
-                    default:
-                        break;
-					}
-				break;
+		case SDL_KEYUP:
+			switch( event->key.keysym.sym ){
+				case SDLK_LEFT:
+                    inputState->Left = UNPRESSED;
+                    break;
+                case SDLK_RIGHT:
+                    inputState->right = UNPRESSED;
+                    break;
+                default:
+                    break;
+				}
+			break;
 
-			case SDL_KEYUP:
-				switch( event->key.keysym.sym ){
-					case SDLK_LEFT:
-                        inputState->Left = UNPRESSED;
-                        break;
-                    case SDLK_RIGHT:
-                        inputState->right = UNPRESSED;
-                        break;
-                    default:
-                        break;
-					}
-				break;
-
-			
-			default:
-				break;
-		}
-    }
+		
+		default:
+			break;
+	}
+//    }
 }
