878e58c0fea9f5d2a73b4404ff6ddfd72d6ca0ad
Veeti Väisänen <veetiv.aisanen1@gmail.com>
2023-04-03 18:55:07 +0300

    Update buildscript

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 
 set -xe
 
-gcc -Wall -Wextra -o example examples/example.c src/event.c src/input.c src/physics.c src/array_list.c src/renderer.c src/glad.c src/io.c -lSDL2main -lSDL2 -lSDL2_image -lm
+echo 'Building breakout clone example'
+# gcc -Wall -Wextra -o example examples/example.c src/event.c src/input.c src/physics.c src/array_list.c src/renderer.c src/glad.c src/io.c -lSDL2main -lSDL2 -lSDL2_image -lm
+
+echo '----Building physics test----'
+# gcc -Wall -Wextra -o physics_test examples/physics_test.c src/glad.c src/event.c src/input.c src/io.c src/physics.c src/array_list.c src/renderer.c -lSDL2main -lSDL2 -lSDL2_image -lm 
+
